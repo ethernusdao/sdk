c0e172886c5c7c60b6f882b9d404fb2fbc8e3c20
Rodrigo Burgos <rodrigoburgos@live.com>
2025-02-03 15:38:43 -0300

    sdk-000: exporting consts aswell

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from "./infrastructure/service/ethernus-service";  // Export your main class
 export * from "./shared/utils/format-decimals";  // Export utility functions
 export * from "./domain/errors/ethernus-errors";  // Export errors
+export * from "./shared/constants/index";  // Export constants
