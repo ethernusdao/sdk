4cefc8870c74ccdc93637574550a229886692800
Rodrigo Burgos <rodrigoburgos@live.com>
2025-02-03 15:16:00 -0300

    sdk-000: trying to publish

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export * from "./infrastructure/service/ethernus-service";  // Export your main class
+export * from "./shared/utils/format-decimals";  // Export utility functions
+export * from "./domain/errors/ethernus-errors";  // Export errors
